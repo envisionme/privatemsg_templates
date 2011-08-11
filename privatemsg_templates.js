ee74e914a754f2fd5ca50384d9814d3d421d9e0d
Ivan Breet <ivanbreet@gmail.com>
2011-08-11 09:06:30 -0700

    Coder assisted cleanup

--- privatemsg_templates.js
+++ privatemsg_templates.js
@@ -1,22 +1,16 @@
 Drupal.behaviors.privatemsg_templates = function(context) {
-  $('#edit-privatemsg-templates', context).bind('change', function(){    
+  $('#edit-privatemsg-templates', context).bind('change', function() {    
 	var nid = this.selectedIndex;
 	//this check is to prevent an unnecessary get request
 	//TODO: add prompt to prevent the user losing a message they may have typed already.
-	if(nid > 0){
-		
-		$.get('/privatemsg_templates_get_template/'+ nid, null, privatemsg_load_template);
-	}else{
+	if(nid > 0) {
+		$.get('/privatemsg_templates/get_template/' + nid, null, privatemsg_load_template);
+	} else {
 		$('#privatemsg-new #edit-body').html('');
 	}
     return false;
   });
 }
-
-/*function privatemsg_select_template(nid){
-	alert('asd');
-	$.get('/privatemsg_templates_get_template/'+ nid, null, privatemsg_load_template);
-}*/
 
 var privatemsg_load_template = function(response) {
   var result = Drupal.parseJson(response);
