a38843432de9a7c303a7ef74b05e56afd6fe5d8e
Ivan Breet <ivanbreet@gmail.com>
2011-08-11 11:14:56 -0700

    Disable message textarea till AJAX finished loading

--- privatemsg_templates.js
+++ privatemsg_templates.js
@@ -4,11 +4,13 @@ Drupal.behaviors.privatemsg_templates = function(context) {
     //this check is to prevent an unnecessary get request
     //TODO: add prompt to prevent the user losing a message they may have typed already.
     if(nid > 0) {
+      $('#privatemsg-new #edit-body').attr("disabled", "disabled");
       $.getJSON('/privatemsg_templates/get_template/' + nid, function(json) {
-        $('#privatemsg-new #edit-body').html(json.data);
+        $('#privatemsg-new #edit-body').val(json.data);
+        $('#privatemsg-new #edit-body').removeAttr("disabled");
       });
     } else {
-      $('#privatemsg-new #edit-body').html('');
+      $('#privatemsg-new #edit-body').val('');
     }
     return false;
   });
