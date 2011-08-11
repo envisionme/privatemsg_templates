42f273922d031410dc7ed2f4920afc15a3bac91f
Ivan Breet <ivanbreet@gmail.com>
2011-08-11 10:38:09 -0700

    Security and other bug fixes

--- privatemsg_templates.js
+++ privatemsg_templates.js
@@ -1,18 +1,15 @@
 Drupal.behaviors.privatemsg_templates = function(context) {
-  $('#edit-privatemsg-templates', context).bind('change', function() {    
-	var nid = this.selectedIndex;
-	//this check is to prevent an unnecessary get request
-	//TODO: add prompt to prevent the user losing a message they may have typed already.
-	if(nid > 0) {
-		$.get('/privatemsg_templates/get_template/' + nid, null, privatemsg_load_template);
-	} else {
-		$('#privatemsg-new #edit-body').html('');
-	}
+  $('select#edit-privatemsg-templates', context).bind('change', function() {    
+    var nid = $(this).val();
+    //this check is to prevent an unnecessary get request
+    //TODO: add prompt to prevent the user losing a message they may have typed already.
+    if(nid > 0) {
+      $.getJSON('/privatemsg_templates/get_template/' + nid, function(json) {
+        $('#privatemsg-new #edit-body').html(json.data);
+      });
+    } else {
+      $('#privatemsg-new #edit-body').html('');
+    }
     return false;
   });
-}
-
-var privatemsg_load_template = function(response) {
-  var result = Drupal.parseJson(response);
-  $('#privatemsg-new #edit-body').html(result.data);
 }
